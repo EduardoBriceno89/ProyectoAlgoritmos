de5886040c7f7b57c97a95eba85af04d252ef8f8
PapsChulo <83840504+EduardoBriceno89@users.noreply.github.com>
2022-03-30 10:30:21 -0600

    Update main.py

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,24 +1,15 @@
-from areas import AreaTriangulo
+class Areas:
+    def __init__(self, base, altura, diagonalmay, diagonalmen):
+        self.base = base
+        self.altura = altura
+        self.diagonalmay = diagonalmay
+        self.diagonalmen = diagonalmen
+    
+    def areaTriangulo(self):
+        return (self.base * self.altura)/2
+    
+    def areaRectangulo(self):
+        return self.base * self.altura
 
-opcion = None
-
-print("Escoja las opciones a continuacion...")
-print("1- Areas")
-print("2- Volumenes")
-print("3- Distancias")
-print("4- Salir")
-
-opcion = int(input("Selecciona una opcion: "))
-
-while opcion != 4:
-
-    if opcion == 1:
-        print("Areas".center(50,'*'))
-
-        valor1 = float(input("Ingrese base: "))
-        valor2 = float(input("Ingrese altura: "))
-
-        area = AreaTriangulo(valor1, valor2)
-
-        print(f"El area del cuadrado es: {area.areaTriangulo()}")
-
+    def areaRombo(self):
+        return (self.diagonalmay * self.diagonalmen)
